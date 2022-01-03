b2ca8a0537bac4cc9ca79d9fd96d58c97b66777b
EddyVinck <eddyvinck95@gmail.com>
2022-01-03 22:02:35 +0100

    feat: make it uppercase

--- src/js/logloop.ts
+++ src/js/logloop.ts
@@ -1,5 +1,5 @@
 export function logLoop(loops: number) {
   for (let index = 0; index < loops; index++) {
-    console.log("hey from typescript");
+    console.log("hey from typescript".toUpperCase());
   }
 }
